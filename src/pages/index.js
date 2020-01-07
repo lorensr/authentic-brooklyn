e3bb29c3554638b1c2b35eddcd32fcdc0504f7fd
Loren ☺️ <251288+lorensr@users.noreply.github.com>
2020-01-07 17:11:14 -0500

    Add lexicon link

--- src/pages/index.js
+++ src/pages/index.js
@@ -27,6 +27,13 @@ function IndexPage() {
             >
               More info
             </a>
+            and
+            <a
+              className="text-blue-400 whitespace-no-wrap"
+              href="https://docs.google.com/document/d/1ELhGwpar0_c-4zjIDbv9Qq4Sn2axQOxEbDhkKQwbJa8/edit"
+            >
+              full lexicon
+            </a>
             .
           </p>
 
